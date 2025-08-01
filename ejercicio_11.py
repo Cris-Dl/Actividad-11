ce04842fc9813b02cbbff2f070d25a662fab1ba6
Cristhian de León <deleonperezcristhianestuardo@gmail.com>
2025-08-01 00:35:50 -0600

    Se realizaron las correcciones al mostrar el resumen general y se hizo la busqueda del propietario

--- ejercicio_11.py
+++ ejercicio_11.py
@@ -1,5 +1,4 @@
 propietarios = {}
-placas = {}
 while True:
     print("--Menú--")
     print("1.- Ingresar propietario")
@@ -18,14 +17,15 @@ while True:
                 nombre = str(input("Ingrese el nombre del propietario: "))
                 telefono = int(input("Ingrese el número de telefono: "))
                 cantidad_vehiculos = int(input("Ingrese la cantidad de vehiculos a registrar: "))
+                placas = {}
                 for i in range(cantidad_vehiculos):
                     placa = str(input("Ingrese la placa del vehiculo: "))
                     marca = str(input("Ingrese la marca del vehiculo: "))
                     modelo = str(input("Ingrese el modelo del vehiculo: "))
                     ano_vehiculo = int(input("Ingrese el año del vehiculo: "))
                     pago = str(input("Se tiene pagado el impuesto del vehiculo (Si/No)?: "))
-                    propietarios[nit] = {"nombre":nombre, "telefono":telefono}
-                    placas[placa] = {"marca":marca, "modelo":modelo, "ano":ano_vehiculo, "pago":pago}
+                    placas[placa] = {"marca": marca, "modelo": modelo, "ano": ano_vehiculo, "pago": pago}
+                    propietarios[nit] = {"nombre":nombre, "telefono":telefono, 'carros':placas}
                     print()
             print("Se ha hecho el registro correctamente")
             print()
@@ -36,10 +36,19 @@ while True:
                 print(f"Nombre: {datos['nombre']}")
                 print(f"Telefono: {datos['telefono']}")
                 print()
-            for placa, data in placas.items():
-                        print(f"Placa: {placa}, Marcas:{data['marca']}, Modelo:{data['modelo']}, Año: {data['ano']}, Pago: {data['pago']}")
+                for placa, data in datos['carros'].items():
+                    print(f"Placa: {placa}, Marca:{data['marca']}, Modelo:{data['modelo']}, Año: {data['ano']}, Pago: {data['pago']}")
+                    print()
         case "3":
             print("Buscar propietario")
+            nit_busqueda = int(input("Ingrese el NIT del propietario: "))
+            if nit_busqueda in propietarios:
+                print(f"Nombre: {propietarios[nit_busqueda]['nombre']}")
+                print(f"Telefono: {propietarios[nit_busqueda]['telefono']}")
+            for placa, data in propietarios[nit_busqueda]['carros'].items():
+                print("Información del vehiculo")
+                print(f"Placa: {placa}, Marca:{data['marca']}, Modelos:{data['modelo']}, Año:{data['ano']}, Pago:{data['pago']}")
+                print()
         case "4":
             print("Verificación de pago")
         case "5":
